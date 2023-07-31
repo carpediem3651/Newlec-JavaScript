772f540385a26356e37c2315b828dd93ab7d863d
NamKiHyeok <carpediem7490@gmail.com>
2023-07-31 14:21:16 +0900

    23.07.31 Lecture: Rest Parameter, Spread Operator, Arrow Function,bind/call/apply

--- ES6/es6.js
+++ ES6/es6.js
@@ -149,7 +149,7 @@
 
 {   // -------------**********이중배열 뽀개기-------------------
     let kors = [[20,30,40,30],[40,30,20,30]];
-    let [[kor1, kor2],[kor3, kor4]] = kors;
+    let [[kor1,kor2,,],[kor3,kor4,,]] = kors;
     // console.log(kor1,kor2,kor3,kor4);
 }
 
@@ -161,9 +161,206 @@
 
     ({kor:kor1, eng, kors:[,,kor2]} = exam);
 
-    console.log(kor1, eng, kor2);
+    // console.log(kor1, eng, kor2);
 }
 
+
+// ================ Rest Parameter 이용하기 위한 매개값 전달 ==================
+{
+    // function print(x,y, ...aa) {
+    //     console.log(x,y,aa[0],aa.length,arguments[0],arguments[2],arguments.length); // 비교를 통해 개념을 익혀보자.
+    // }
+
+    // print(3,4,5,6,7);
+}
+
+// ================ Spread Operator 이용하기 위한 매개값 전달 ==================
+// Spread: 배열을 늘어뜨린다. 배열의 인자들을 하나의 요소들로 늘어뜨린다.
+{
+    // function print1(nums) {
+    //     console.log(nums);
+    // }
+
+    // function print2(nums1,nums2,nums3,nums4) {
+    //     console.log(nums1,nums2,nums3,nums4)
+    // }
+
+    // let nums = [4,34,23,12];
+
+    // print1(nums);
+    // // print2(...nums);
+
+    // console.log('-----------사용 예-----------');
+    // let nums2 = [...nums,1,3,2]; // 배열안의 배열을 하나의 배열로 표시하기에 유용하다.
+    // console.log(nums2); 
+
+    // console.log('-----------복합 예-----------');
+    
+    // function print2([x,y,z]) {
+    //     console.log(x,y,z);
+    // }
+
+    // print2(nums);
+
+    // function print3({kor,eng,math}) {
+    //     console.log(kor,eng,math);
+    // }
+
+    // let exam = {kor:30, eng:20, math:80};
+    // print3(exam);
+}
+
+// ================ 함수의 기본 값 ==================
+{
+    // // case 1.기본
+    // function print(x,y=10,z) {
+    //     console.log(x,y,z);
+    //     console.log(arguments.length); // 결과값 : 1 -> aruments는 기본값(y=10)을 받지 않는다.'
+        
+    // }
+
+    // print(10,null,10) // null은 원시 '값'이다 값이기 때문에 default value로 y=10이 작동하지 않는다.
+    // print(10,undefined, 10); // undefiend은 값이 들어있지 않은 통이라고 생각하자. 그러면 왜 10이 나오는지 알 수 있다.
+    // print(1);
+
+    // // case 2.초기값 함수
+    // function sum() {
+    //     return 100;
+    // }
+
+    // function print(x,y=sum(),z) {
+    //     console.log(x,y,z);
+    //     console.log(arguments.length); // 결과값 : 1 -> aruments는 기본값(y=10)을 받지 않는다.'
+        
+    // }
+
+    // print(10,null,10) // null은 원시 '값'이다 값이기 때문에 default value로 y=10이 작동하지 않는다.
+    // print(10,undefined, 10); // undefiend은 값이 들어있지 않은 통이라고 생각하자. 그러면 왜 10이 나오는지 알 수 있다.
+    // print(10,undefined);
+    // print(1);
+
+    // // case 3. 연산한 값을 매개변수로 쓰기
+    // function sum() {
+    //     return 100;
+    // }
+
+    // function print(x,y=sum(),z=x=1) {
+    //     console.log(x,y,z);
+    //     console.log(arguments.length); // 결과값 : 1 -> aruments는 기본값(y=10)을 받지 않는다.'
+        
+    // }
+
+    // print(10,null,10) // null은 원시 '값'이다 값이기 때문에 default value로 y=10이 작동하지 않는다.
+    // print(10,undefined, 10); // undefiend은 값이 들어있지 않은 통이라고 생각하자. 그러면 왜 10이 나오는지 알 수 있다.
+    // print(10,undefined);
+    // print(1);
+}
+
+// ==================Arrow Function====================
+{
+    //---------------- Arrow Function의 기본사용--------------------
+
+    // let nums = [10,20,29,82,39,28,100];
+    // nums.sort(function(a,b){
+    //     return a-b;
+    // });
+    // console.log(nums);
+
+    // nums.sort((a,b)=>a-b);
+    // console.log(nums);
+
+    //-------------------  차이점1: Arrow Function은 this를 가지지 않는다.--------------------
+    // let x = 40;
+
+    // setTimeout(function(){
+    //     console.log(x);
+    // },1000);
+
+    // // let game = {
+    // //     x:30,
+    // //     run(){
+    // //         setInterval(function(){
+    // //             this.x++; // this가 window 객체를 가지고 있기 때문(window객체에 x값 정의 안됨)에 NaN이 뜬다.
+    // //             console.log(this.x);
+    // //         }, 1000);
+    // //     }
+    // // };
+
+    // let game = {
+    //     x:30,
+    //     run(){
+    //         setInterval(()=>{
+    //             this.x++; // Arrow Function은 this를 가지고 있지 않기 때문에 x값을 활용할 수 있다.
+    //             console.log(this.x);
+    //         }, 1000);
+    //     }
+    // };
+
+    // game.run();
+
+    // --------------------------차이점2 : Arrow Function은 arguments가 없다. --------------------------
+    // let print() = function(){
+    //     console.log(arguments[0],arguments[1]) // 결과값 :2,3
+    // }
+    
+    // let print = () => {
+    //     console.log(arguments[0], arguments[1]); // 결과값 : error
+    // }
+
+    // print(2,3);
+
+    // --------------------------차이점 3 : Arrow Function은 생성자로 사용할 수 없다. this 관련 행위를 가지지 않는다. --------------------------
+    // let print = function(){
+    //     console.log('normal function', this); 
+    // };
+    
+    // let print1 = () => {
+    //     console.log('arrow function', this); 
+    // }
+
+    // print(2,3);
+    // new print(2,3);
+
+    // print1(2,3);
+    // new print1(2,3); // 결과: Uncaught TypeError: print1 is not a constructor
+}
+
+    // ----------------------- 차이점 4: call / bind / aplly를 사용할 수 없다. ----------------------- 
+{
+
+    //  ---------------- bind() -----------------
+    // let game = {
+    //     x:30 ,
+    //     run() {
+    //         console.log(this); // this는 game 객체를 가리킴 -> x:30이 출력됨
+    //         setTimeout(function(){
+    //             console.log(this); // this는 가리키는 객체가 없기 때문에 window객체를 나타냄
+    //             this.x++; // window객체에 x속성이 없으므로 NaN에러
+    //             console.log(this.x);
+    //         }.bind(this), 1000); // bind()를 써서 외부 함수의 속성(x)을 끌어오는 문제를 해결. this는 game객체.
+    //     }
+    // };
+
+    // // game.run();
+
+
+    // // ----------------- call() & apply ---------------------
+    // let f = function(a,b) {
+    //     console.log(a,b);
+    //     console.log(this);
+    //     this.x++;
+    //     console.log(this.x);
+    // };
+
+    // let game2 = {
+    //     x:10
+    // }
+
+    // f.call(game,100,200); // f함수를 호출할 때 call()을 써서 매개변수를 변화 시킬 수 있다. game.f(); / game2.f(); 불가능 결과값: 31
+    // f.call(game2);
+    // f.apply(game2,[100,200]);
+    // f();
+}
 
 
 
